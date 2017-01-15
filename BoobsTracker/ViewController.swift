fc13d16600185fcb58d71e5803c4fa5e11ca9982
Ilja Stepanow <hyston@Iljas-MBP.fritz.box>
2017-01-15 11:08:47 +0100

    new project

---
+++ BoobsTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoobsTracker
+//
+//  Created by Ilja Stepanow on 15/01/2017.
+//  Copyright © 2017 Ilja Stepanow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
